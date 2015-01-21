3d6c0d916fbca00c5ef08925c399e880923c3cb6
Y.Takanashi <Y.R.Takanashi@gmail.com>
2015-01-21 17:48:14 +0900

    Ver.9.22.00.01+

--- Src/C/7zVersion.h
+++ Src/C/7zVersion.h
@@ -1,8 +1,8 @@
 #define MY_VER_MAJOR 9
 #define MY_VER_MINOR 22
 #define MY_VER_BUILD 00
-#define MY_VERSION "9.22 beta + force utf8 patch"
-#define MY_7ZIP_VERSION "9.22 beta + force utf8 patch"
+#define MY_VERSION "9.22 beta + ungarbled patch"
+#define MY_7ZIP_VERSION "9.22 beta + ungarbled patch"
 #define MY_DATE "2012-05-23"
 #define MY_COPYRIGHT ": Igor Pavlov : Public domain"
 #define MY_VERSION_COPYRIGHT_DATE MY_VERSION " " MY_COPYRIGHT " : " MY_DATE
